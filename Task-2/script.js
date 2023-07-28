789b22108b7967fe5b03885c157f0e57805a96d0
Priiyyya-21 <priyamajhi79@gmail.com>
2023-07-28 21:26:00 +0530

    Created Portfolio

---
+++ Task-2/script.js
@@ -0,0 +1,74 @@
+// scroll bar
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Event handling side menu
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu() {
+  sidemenu.style.right = "0";
+}
+
+function closeMenu() {
+  sidemenu.style.right = "-200px";
+}
+
+//  Click
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzAJxJeBlbsUxc9Cc7aUEcbegTJ8vx5o5UmKk3_hgtU1OiBtcSdpSgyywRwmj2VROLPPw/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => {
+      msg.innerHTML = "Oops... Something went wrong!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    });
+});
